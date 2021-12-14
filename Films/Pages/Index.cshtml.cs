5ea00cb48f95b2349bdf3cd8f7947066231b2749
Alekhya <A00245382@mycambrian.ca>
2021-12-13 23:24:59 -0500

    first commit

---
+++ Films/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Films.Pages.Models;
+
+namespace Films.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<FilmsModal> lists = new List<FilmsModal>();
+        public IndexModel()
+        {
+                 FilmsModal obj = new FilmsModal();
+                obj.Title = "Movie Title : RRR";
+                obj.Description = "Description : This is a fictional movie which revolves around two Indian freedom fighters Alluri Seetarama Raju and Komaram Bheem" +
+                "Cast: NTR,Ram Charan,AaliaBhatt" +
+                "Director: S S Rajamouli" +
+                "Music Director: M M Keeravani";
+                obj.Duration = 120 ;
+                lists.Add(obj);
+        }
+        public void OnGet()
+        {
+
+        }
+        
+    }
+   
+}
---
+++ Films/Pages/Models/FilmsModal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Films.Pages.Models
+{
+    public class FilmsModal
+    {
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public int Duration { get; set; }
+        
+    }
+}
